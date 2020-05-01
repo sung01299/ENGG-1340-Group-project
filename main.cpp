978c59d1be9feb942edaf5f5dd6eeb9009dd5570
Choi Yoon Sun <h3568378@academy11.cs.hku.hk>
2020-05-01 15:51:13 +0800

    Added struct information for player, enemy, item, skill information

--- main.cpp
+++ main.cpp
@@ -4,11 +4,69 @@ using namespace std;
 
 int main()
 {
+
+    /* This struct is used to store player's information
+        pname for player's name
+        maxHealth for player's health
+        currentHealth for player's current health
+        basicAttack for the basic amount of damage the player can deal;
+        pdefense for player's current health
+        invenGold for amound of gold the player has
+        currentLoc for current location of the player
+        inven for player's inventory and the maximum number of items the player can hold is 5
+        obtainedSkill for the skills the player has and the maximum number of skills players can use*/
+    struct PlayerInfo {
+        string pname;
+        int maxHealth;
+        int currentHealth;
+        int pdefense;
+        int basicAttack;
+        int invenGold;
+        int currentLoc;
+        string inven[5];
+        string obtainedSkill[3];
+    };
+
+    /* This struct is used to store enemy information 
+        ename for enemy's name
+        ehealth for enemy's health
+        edefense for enemy's defense
+        egold for the amount of gold player could ontain from defeating it */
+    struct EnemyInfo {
+        string ename;
+        int ehealth;
+        int edefense;
+        int egold;
+    };
+
+    /* This struct is used to store item information 
+        itemID for ID of the item
+        itemName for name of the item
+        itemPrice for price of the item */
+    struct ItemInfo {
+        string itemID;
+        string itemName;
+        int itemPrice;
+    };
+
+    /* This struct is used to store skill information
+        skillID for ID of the skill
+        skillName for name of the skill
+        damage for the amount of damage the skill can deal
+        maxTimes for the number of times the skill could be used */
+    struct SkillInfo {
+        string skillID;
+        string skillName;
+        int damage;
+        int maxTimes;
+    };
+
     /* variable inputline stores the input from the user
        variable name stores the name of the player/character
        variable answer stores the choice (e.g. 1/0) from the player */
     string inputLine, name, answer;
 
+    /* Main start menu */
     cout << "~~~~~~~~~~ MYSTERIOUS YYETNAL ~~~~~~~~~~" << endl;
     cout << "Developed by CHOI Yoon Sun and HUH Sung." << endl;
     cout << "Type \"start\" to start the game." << endl;
@@ -16,8 +74,8 @@ int main()
 
     /* If the player inputs something other than "start", it will ask the player to input again until it is a valid input (i.e., start)*/
     while (inputLine != "start") {
-        cout << "Invalid input. Please type \"start\" to start the game." << endl;
-        getline(cin, inputLine);
+        cout << "***Invalid input. Please type \"start\" to start the game.***" << endl;
+        cin >> inputLine;
     }
 
     /* Getting name from the player */
@@ -69,6 +127,8 @@ int main()
         }
     }
 
+    PlayerInfo pinfo;
+    pinfo.pname = name;
     cout << endl << "????: My apologies for not introducing myself first." << endl;
     cout << "Noin: The name is Noin Arak, but you may call me Noin." << endl;
 
