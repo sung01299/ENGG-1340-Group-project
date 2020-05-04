ed875c27834db191b3062ebabedb115a781353c0
shuh01 <63391638+shuh01@users.noreply.github.com>
2020-05-04 23:24:30 +0800

    Update main.cpp
    added function "trade and upgrade"

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 
-void battleenemy(){
+void battleenemy(//enemy#){
     srand(time(NULL));
     int answer, retur, missrate, critrate;
     missrate = ??;
@@ -45,7 +45,7 @@ void battleenemy(){
         }
         
         if (pinfo.maxHealth <= 0){
-            cout << "You died." << endl;
+            cout << "Player died." << endl;
             cout << "To return to main menu, please type \"return\"." << endl;
             cin >> retur;
             while (retur != "return"){
@@ -57,12 +57,52 @@ void battleenemy(){
             }
         }
         else if (einfo.ehealth <=0){
-            cout << "You defeated enemy." << endl;
-            cout << "You can now move on to next village." << endl;
+            cout << "Player defeated enemy." << endl;
+            cout << "Enemy dropped its unique part" << endl;
+            
+            cout << "Player can now move on to next town." << endl;
         }
     }
 }
 
+void tradeandupgrade(//enemy#){
+    int answer0, answer1, answer2, answer3;
+    cout << "Player can trade the unique parts or upgrade weapon and armor." << endl;
+    cout << "To trade the unique parts with gold, type \"1\", to upgrade weapon, type \"2\", to upgrade armor, type \"3\", to exit, type \"0\" << endl;
+    cin >> answer0;
+    while (answer0 != 0){
+        if (answer0 == "1"){
+            cout << "You can trade the part obtained from previous battle with " << //value of monster part << ". To trade, type \"1\"." << endl;
+            cin >> answer1;
+            if (answer1 == "1"){
+                pinfo.invenGold += partvalue;
+                cout << "Player currently has " << pinfo.invenGold << " gold." << endl;
+            }
+        }
+        else if (answer0 == "2"){
+            cout << "Player can upgrade weapon with " << amount of increase (attack damage) << " by spending " << price to upgrade << ". To upgrade, type \"1\"." << endl;
+            cin >> answer2;
+            if (answer2=="1"){
+                pinfo.basicAttack += amount of increase (attack damage);
+                pinfo.invenGold -= price to upgrade;
+                cout << "Player's attack damage increased to " << pinfo.basicAttack << endl;
+                cout << "Player currently has " << pinfo.invenGold << " gold." << endl;
+            }
+        }
+        else if (answer0 == "3"){
+            cout << "Player can upgrade armor with " << amount of increase (health) << " by spending " << price to upgrade << ". To upgrade, type \"1\"." << endl;
+            cin >> answer3;
+            if (answer3=="1"){
+                pinfo.pdefense += amount of increase (health);
+                pinfo.invenGold -= price to upgrade;
+                cout << "Player's health increased to " << pinfo.pdefense << endl;
+                cout << "Player currently has " << pinfo.invenGold << " gold." << endl;
+            }
+        }
+    }
+    cout << "Player exits trademarket." << endl;
+}
+    
 int main()
 {
 
