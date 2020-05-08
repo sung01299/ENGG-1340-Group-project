632d89b801dacf04c2627dc76777b52371d751f1
shuh01 <63391638+shuh01@users.noreply.github.com>
2020-05-08 17:50:06 +0800

    Update trade.cpp

--- trade.cpp
+++ trade.cpp
@@ -9,13 +9,13 @@ void tradeandupgrade(){
     cout << "To trade the unique parts with gold, type \"1\", to upgrade weapon, type \"2\", to upgrade armor, type \"3\", to exit, type \"0\"" << endl;
     cin >> answer0;
     while (answer0 != 0){
-        if (answer0 == "1"){
+        if (answer0 == 1){
             cout << "Input numbers of item you would like to sell." << endl;
             cin >> answer1;
             string name;
             cout << "List the name of items that you would like to sell." << endl;
             for (int i=0; i<answer1; i++){
-                cin >> name
+                cin >> name;
                 names[i]=name;
             }
             for (int i=0; i<names.size(); i++){
@@ -23,23 +23,23 @@ void tradeandupgrade(){
                 pinfo.invenGold += value of names[i];
             }
         }
-        else if (answer0 == "2"){
+        else if (answer0 == 2){
             cout << "Player can upgrade weapon with " << 20*pinfo.currentLoc << " by spending " << 100*pinfo.currentLoc << ". To upgrade, type \"1\"." << endl;
             cin >> answer2;
-            if (answer2=="1"){
+            if (answer2==1){
                 pinfo.basicAttack += 20*pinfo.currentLoc;
                 pinfo.invenGold -= 100*pinfo.currentLoc;
                 cout << "Player's attack damage increased to " << pinfo.basicAttack << endl;
-                cout << "Player currently has " << pinfo.invenGold << " gold." << endl;
+                cout << "Player currently has " << pinfo.invenGold << " golds." << endl;
             }
         }
-        else if (answer0 == "3"){
+        else if (answer0 == 3){
             cout << "Player can upgrade armor with " << 20*pinfo.currentLoc << " by spending " << 100*pinfo.currentLoc << ". To upgrade, type \"1\"." << endl;
             cin >> answer3;
-            if (answer3=="1"){
+            if (answer3==1){
                 pinfo.maxHealth += 20*pinfo.currentLoc;
                 pinfo.invenGold -= 100*pinfo.currentLoc;
-                cout << "Player's health increased to " << pinfo.pdefense << endl;
+                cout << "Player's health increased to " << pinfo.maxHealth << endl;
                 cout << "Player currently has " << pinfo.invenGold << " gold." << endl;
             }
         }
